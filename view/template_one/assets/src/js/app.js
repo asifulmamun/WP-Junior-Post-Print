6e3a6078d277ff519397438b7b50149d9d48b630
asifulmamun <asifulmamun@gmail.com>
2024-11-21 17:47:41 +0600

    working perfect but saving img problem

--- view/template_one/assets/src/js/app.js
+++ view/template_one/assets/src/js/app.js
@@ -1,45 +1,6 @@
 // import "./app.css";
-
 import "./app.scss";
-
 console.log("Log app.js");
-
-
-// Print
-// function includeExternalFiles(printWindow) {
-//   // Get all linked CSS files
-//   var cssLinks = document.querySelectorAll('link[rel="stylesheet"]');
-//   cssLinks.forEach(function(link) {
-//       var href = link.getAttribute('href');
-//       printWindow.document.write('<link rel="stylesheet" href="' + href + '">');
-//   });
-
-//   // Get all external JavaScript files
-//   var jsScripts = document.querySelectorAll('script[src]');
-//   jsScripts.forEach(function(script) {
-//       var src = script.getAttribute('src');
-//       printWindow.document.write('<script src="' + src + '"></script>');
-//   });
-// }
-
-// document.getElementById('printBtn').addEventListener('click', function() {
-//   var printContents = document.getElementById('main').innerHTML;
-//   var originalContents = document.body.innerHTML;
-
-//   var printWindow = window.open('', '_blank');
-//   printWindow.document.open();
-//   printWindow.document.write('<html><head><title>Print</title></head><body>' + printContents + '</body></html>');
-//   includeExternalFiles(printWindow);
-//   printWindow.document.close();
-
-//   printWindow.print();
-
-//   printWindow.onafterprint = function() {
-//       printWindow.close();
-//       document.body.innerHTML = originalContents;
-//   };
-// });
-
 
 // Print
 document.getElementById('printBtn').addEventListener('click', function() {
@@ -50,3 +11,161 @@ document.getElementById('printBtn').addEventListener('click', function() {
 document.getElementById("goBackButton").addEventListener("click", function() {
   window.history.back();
 });
+
+
+// https://html2canvas.hertzen.com/
+document.getElementById('download').addEventListener('click', downloadCanvasAsImage);
+function downloadCanvasAsImage() {
+    const scale = 4;
+    html2canvas(document.querySelector("#main"), {
+        scale: scale,
+    }).then(canvas => {
+        // Convert canvas to data URL
+        const dataURL = canvas.toDataURL('image/png');
+
+        // Create a link element
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        
+        // Set the download attribute to specify the file name
+        downloadLink.download = pageTitle;
+        
+        // Simulate a click to trigger the download
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+}
+
+
+
+
+// En to Bn
+function en2bnNumber(number) {
+  var english_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var bangla_numbers = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
+  return number.toString().split('').map(function(digit) {
+      return bangla_numbers[english_numbers.indexOf(digit)];
+  }).join('');
+}
+
+// Determine the suffix for the day
+function getSuffix(day) {
+    switch (day) {
+        case 1: case 21: case 31:
+            return "লা";
+        case 2: case 22:
+            return "রা";
+        case 3: case 23:
+            return "রা";
+        case 4: case 24:
+            return "ঠা";
+        default:
+            return "ই";
+    }
+}
+
+// Calculate Islamic date
+function calculatingIslamicDate(date, adjust) {
+    var today = date ? new Date(+date) : new Date();
+    if (adjust) {
+        today.setDate(today.getDate() + +adjust);
+    }
+
+    var day = today.getDate();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    var m = month + 1;
+    var y = year;
+
+    if (m < 3) {
+        y -= 1;
+        m += 12;
+    }
+
+    var a = Math.floor(y / 100);
+    var b = 2 - a + Math.floor(a / 4);
+
+    if (y < 1583) b = 0;
+    if (y == 1582) {
+        if (m > 10) b = -10;
+        if (m == 10) {
+            b = 0;
+            if (day > 4) b = -10;
+        }
+    }
+
+    var jd = Math.floor(365.25 * (y + 4716)) + Math.floor(30.6001 * (m + 1)) + day + b - 1524;
+
+    b = 0;
+    if (jd > 2299160) {
+        a = Math.floor((jd - 1867216.25) / 36524.25);
+        b = 1 + a - Math.floor(a / 4);
+    }
+
+    var bb = jd + b + 1524;
+    var cc = Math.floor((bb - 122.1) / 365.25);
+    var dd = Math.floor(365.25 * cc);
+    var ee = Math.floor((bb - dd) / 30.6001);
+    day = (bb - dd) - Math.floor(30.6001 * ee);
+    month = ee - 1;
+
+    if (ee > 13) {
+        cc += 1;
+        month = ee - 13;
+    }
+    year = cc - 4716;
+    var wd = ((jd + 1) % 7 + 7) % 7 + 1;
+
+    var iyear = 10631. / 30.;
+    var epochastro = 1948084;
+    var epochcivil = 1948085;
+
+    var shift1 = 8.01 / 60.;
+
+    var z = jd - epochastro;
+    var cyc = Math.floor(z / 10631.);
+    z = z - 10631 * cyc;
+    var j = Math.floor((z - shift1) / iyear);
+    var iy = 30 * cyc + j;
+    z = z - Math.floor(j * iyear + shift1);
+    var im = Math.floor((z + 28.5001) / 29.5);
+
+    if (im == 13) im = 12;
+    var id = z - Math.floor(29.5001 * im - 29);
+
+    return [
+        day,       // calculated day (CE)
+        month - 1, // calculated month (CE)
+        year,      // calculated year (CE)
+        jd - 1,    // julian day number
+        wd - 1,    // weekday number
+        id,        // islamic date
+        im - 1,    // islamic month
+        iy         // islamic year
+    ];
+}
+
+// Write Islamic date
+function writeIslamicDate(date, adjustment) {
+    var wdNames = ["আহাদ", "ইথনিন", "থুলাথা", "আরবা", "খামস", "জুমু'আহ", "সাবত"];
+    var iMonthNames = ["মহররম", "সফর", "রবিউল আউয়াল", "রবিউস সানি", "জমাদিউল আউয়াল", "জমাদিউস সানি",
+        "রজব", "শাবান", "রমজান", "শাওয়াল", "জিলকদ", "জিলহজ"];
+    var iDate = calculatingIslamicDate(date, adjustment);
+    var outputIslamicDate = en2bnNumber(iDate[5]) + getSuffix(iDate[5]) + " " + iMonthNames[iDate[6]] + " " + en2bnNumber(iDate[7]);
+    return outputIslamicDate;
+}
+
+// Get today's date
+var today = new Date();
+var day = today.getDate();
+var monthNames = ["জানুয়ারী", "ফেব্রুয়ারী", "মার্চ", "এপ্রিল", "মে", "জুন", "জুলাই", "আগস্ট", "সেপ্টেম্বর", "অক্টোবর", "নভেম্বর", "ডিসেম্বর"];
+var month = monthNames[today.getMonth()];
+var year = today.getFullYear();
+
+var banglaDayString = new buetDateConverter().convert("l");
+var banglaDateString = new buetDateConverter().convert("d F, Y");
+document.getElementById('banglaDayString').textContent = 'প্রিন্ট এর তারিখঃ ' + banglaDayString;
+document.getElementById('banglaDateString').textContent = banglaDateString;
+document.getElementById('englishDateString').textContent = en2bnNumber(day) + getSuffix(day) + " " + month + " " + en2bnNumber(year);
+document.getElementById('arabicDateString').textContent = writeIslamicDate(new Date(), 0);
